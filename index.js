613153557de42f7a6e87779806d3d0946cf10f1b
faithadewuyi <iyanuleke09@gmail.com>
2024-04-21 17:59:10 +0100

    feat: styling added

---
+++ index.js
@@ -0,0 +1,7 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        // Getting authors name from API
+        document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
