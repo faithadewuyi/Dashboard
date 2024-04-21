8737a1484d202b7c26b5620a504920104c3b7443
faithadewuyi <iyanuleke09@gmail.com>
2024-04-21 22:09:16 +0100

    Bitcoin Name and Icon added

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
 
+    .catch(err => {
+      document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+    
+    document.getElementById("author").textContent =`By:Dodi Achmad`
+    })
     //Getting Cypto Data
 
     fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
+    .then(res => {
+      if (!res.ok){
+        throw Error("Something not right")
+      }
+     return res.json()
+    })
+    .then(data => 
+      {
+        document.getElementById("bitcoin-top").innerHTML =`
+        <img src=${data.image.small}/>
+        <span>${data.name}</span>`
+      })
     .catch(err => console.error(err))
