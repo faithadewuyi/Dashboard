56855e311931ca8e3c9e7ed93447286575b851f8
faithadewuyi <iyanuleke09@gmail.com>
2024-04-21 21:41:53 +0100

    Adding crypto Api

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         // Getting authors name from API
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
+    //Getting Cypto Data
+
+    fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
